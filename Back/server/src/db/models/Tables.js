fc5fea75ce68f8d5c39f0712520e0faa5fa78df4
victor <victor.lavado15@hotmail.com>
2023-05-08 14:12:46 -0500

    Add model Tables

---
+++ Back/server/src/db/models/Tables.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
+
+const TablesScheme = new mongoose.Schema(
+  {
+    diners: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
+      autopopulate: true,
+    },
+
+    orders: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Posts",
+        autopopulate: true,
+      },
+    ],
+  },
+  {
+    versionKey: false,
+  }
+);
+
+CursosScheme.plugin(mongoosePaginate);
+CursosScheme.plugin(require("mongoose-autopopulate"));
+
+module.exports = mongoose.model("Tables", TablesScheme);
